f82a4f7abea2e45f1e74912dbc94940d734ee766
ThiagoDiniz-creator <thiago.diniz.20.03.2002@gmail.com>
2021-11-16 10:36:02 -0300

    type rating functionality completed

--- src/app.js
+++ src/app.js
@@ -1,19 +1,38 @@
 const express = require("express");
 
+const { getPokemonById } = require("../utils/pokemonList.js");
+const type = require("../utils/individual/type");
 const app = express();
 
 app.get("/pokemon", (req, res) => {
-
-    console.log(req.query);
-
-  if (req.query.id === undefined || req.query.id === "" || isNaN(req.query.id)) {
+  if (
+    req.query.id === undefined ||
+    req.query.id === "" ||
+    isNaN(req.query.id)
+  ) {
     res.send({
       error: "Please provide a valid id",
     });
   } else {
     const { id } = req.query;
+    const pokemon = getPokemonById(id);
+    const { types, name, stats } = pokemon;
+    const typeRating = type.getTypeRating(
+      types.length > 1
+        ? {
+            type1: types[0].type.name,
+            type2: types[1].type.name,
+            quantity: 2,
+          }
+        : {
+            type1: types[0].type.name,
+            quantity: 1,
+          }
+    );
     res.send({
       id,
+      name: pokemon.name,
+      typeRating,
     });
   }
 });
